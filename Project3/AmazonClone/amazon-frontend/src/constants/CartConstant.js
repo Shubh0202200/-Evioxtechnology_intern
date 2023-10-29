53f8846e021ef6950000653f887cab43cea018c4
Shubh0202200 <100746021+Shubh0202200@users.noreply.github.com>
2023-10-29 22:15:45 +0530

    Update CartConstant.js

--- Project3/AmazonClone/amazon-frontend/src/constants/CartConstant.js
+++ Project3/AmazonClone/amazon-frontend/src/constants/CartConstant.js
@@ -1,61 +1,8 @@
-import React from 'react'
-import StarIcon from '@material-ui/icons/Star';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import StarHalfIcon from '@material-ui/icons/StarHalf';
-import "../styles/Rating.css"
-import Product from './Product';
+export const CART_ADD_ITEM = "CART_ADD_ITEM";
+export const CART_REMOVE_ITEM = "CART_REMOVE_ITEM";
+
+export const CART_SAVE_SHIPPING_ADDRESS = "CART_SAVE_SHIPPING_ADDRESS";
+export const CART_SAVE_PAYMENT_METHOD = "CART_SAVE_PAYMENT_METHOD";
 
 
-const Rating = (props) => {
-    
-    const {rating, numRev} = props;
-
-    return (
-        <div className="rating-container">
-
-            <div className="rating">
-                { (rating>=1)
-                ? <StarIcon/>
-                : (rating === 0.5)
-                ? <StarHalfIcon/>
-                : <StarBorderIcon/>
-                }
-
-                { (rating>=2)
-                ? <StarIcon/>
-                : (rating === 1.5)
-                ? <StarHalfIcon/>
-                : <StarBorderIcon/>
-                }
-
-                { (rating>=3)
-                ? <StarIcon/>
-                : (rating === 2.5)
-                ? <StarHalfIcon/>
-                : <StarBorderIcon/>
-                }
-
-                { (rating>=4)
-                ? <StarIcon/>
-                : (rating === 3.5)
-                ? <StarHalfIcon/>
-                : <StarBorderIcon/>
-                }
-
-                { (rating===5)
-                ? <StarIcon/>
-                : (rating === 4.5)
-                ? <StarHalfIcon/>
-                : <StarBorderIcon/>
-                }
-            </div>
-
-            <span>{numRev} reviews</span>
-
-
-        </div>
-        
-    )
-}
-
-export default Rating
+export const CART_EMPTY = "CART_EMPTY";
